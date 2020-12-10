768ec8ea0249ca611f47bf08e4eae38b8269ee8d
Marco Nekoula <mnekoula@uncc.edu>
2020-12-10 17:48:20 -0500

    First commit

---
+++ src/calcsolve/CalcSolve.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calcsolve;
+
+/**
+ *
+ * @author marco
+ */
+public class CalcSolve {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        frame1 intro = new frame1();
+        intro.setSize(410, 325);
+        intro.setVisible(true);
+        
+    }
+    
+}
